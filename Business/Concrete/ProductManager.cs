bbbc8462eda12c04082f59531089f4d1694dae7a
NURDOGAN YAVUZ <nurdogan.yavuz@ogrenci.karatay.edu.tr>
2021-02-18 00:11:47 +0300

    18.02.2021-Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspect.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,12 +23,11 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof (ProductValidator))]
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid); //girilen ürün ismi 2 karakterden az ise ProductNameInvalid mesajını döndürecek, ve eklemeyecek.
-            }
+            
+
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded); //girilen ürün eklendiğinde ProductAdded mesajını döndürecek.
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -15,10 +15,10 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        IProductService _productService;
+        IProductService _productService; //bu servise bağımlılığı olduğu için injection yaptık.
 
-        public ProductsController(IProductService productService)
-        {
+        public ProductsController(IProductService productService) //soyut sınıfa bağlı yani zayıf bağımlılık var -> loosely coupled
+        {                                                        //Yani Iproductservice'e bağımlı ama soyut bir class oldugu için zayıf bağımlılık var. Dolayısıyla Manager'da değişiklik olursa burada bir sorun yaşamayacagız.
             _productService = productService;
         }
 
@@ -29,9 +29,9 @@ namespace WebAPI.Controllers
             var result = _productService.GetAll();
             if (result.Success)
             {
-                return Ok(result);
-            }
-            return BadRequest(result);
+                return Ok(result.Data); //result'ın success'i true ise Ok durumunu döndürür. Ok -> 200 ok -> requestin basarılı oldugunu belirtir. 
+            }                          //result.data -> succes true ise result'ın sadece datasını döndürür, success ya da message gelmez.
+            return BadRequest(result.Message); //result'ın succes'i false ise BadRequest döndürür. result.message yaptıgımız için success false ise result'ın sadece mesajını döndürür.
            
         }
 
@@ -43,7 +43,7 @@ namespace WebAPI.Controllers
             {
                 return Ok(result);
             }
-            return BadRequest(result);
+            return BadRequest(result); //yukarıdaki gibi result.data, result.message şeklinde de kullanmadıgımız için result'ın tüm degerlerini getirir -> success, message, data.
         }
 
         [HttpPost("add")]
@@ -58,3 +58,32 @@ namespace WebAPI.Controllers
         }
     }
 }
+
+//NOTLAR
+
+//IProductService _productService; 
+
+//public ProductsController(IProductService productService){
+//    _productService = productService;
+//}
+
+//Burada ctor içine IProductService verdik, yani controller çalışmaya basladıgında IProductService'i implement eden bir class istiyoruz. 
+//program bunun productmanager oldugunu çözümleyemez. çünkü birden fazla class IProductService'i implement edebilir. hangisi oldugunu bilemez.
+//burada direkt productmanager verseydik, yani somut class. ona bagımlı olmus olurduk. BUNUN YERİNE;
+//IoC Container -- Inversion of control yapısını kullanırız. ctor'daki soyut sınıf parametresinin arkaplanda hangi class'ın referansını tutuyorsa
+//o class'ın newlenmiş hali IoC'de tutulur. ProductsController çalıştıgında IoC kutusu kontrol edilir ve 
+//ctor'daki Iproductservice hangi classın referansını tutuyorsa o getirilir. böylece controller ıproductservice'i çözümlemiş olur.
+
+
+//STARTUP NOT
+//IoC yapısı
+
+//services.AddSingleton<IProductService, ProductManager>(); *1*
+
+//biri ctor'da IProductService isterse; ona (IoC'de tutulan) newlenmiş ProductManager verilir.
+//yani AddSingleton; programın herhangi bir yerinde Iproductservice istenildiğinde, arkaplanda ProductManager'ı newleyip istenilen yere gönderiyor.
+
+//services.AddSingleton<IProductDal, EfProductDal>(); *2*
+
+//eğer birisi senden IProductDal isterse, arkapalanda onun için bir EfProductDal newle ve gönder diyoruz programa.
+
